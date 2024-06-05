4337deeed2dca20b858e40d43ce1a68c952ed29c
avivilloz <avivilloz@gmail.com>
2024-06-05 14:08:44 +0300

    ur

--- infra/pipeline.sh
+++ infra/pipeline.sh
@@ -3,6 +3,8 @@
 . vars.sh
 
 terraform -chdir="$infra_base_dir/provision" init
-terraform -chdir="$infra_base_dir/provision" apply -auto-approve -var="project_name=$project_name"
+terraform -chdir="$infra_base_dir/provision" apply -auto-approve \
+    -var="project_name=$project_name"
 
-ansible-playbook "$infra_base_dir/deployment/deployment.yaml" -e "project_name=$project_name"
+ansible-playbook "$infra_base_dir/deployment/deployment.yaml" \
+    -e "project_name=$project_name"
