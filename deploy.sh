e6129d68019d0be683b8a65f88de4574578f3f2b
avivilloz <avivilloz@gmail.com>
2024-07-02 12:14:40 +0300

    ur

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ ansible_deploy_path="infra/ansible/website_deploy.yaml"
 terraform -chdir=$terraform_production_env_path init
 terraform -chdir=$terraform_production_env_path apply -auto-approve
 
-# ansible-playbook $ansible_deploy_path
+ansible-playbook $ansible_deploy_path
