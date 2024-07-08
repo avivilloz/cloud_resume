2f34d2f4062f196014a87ea7435d5d3485b28146
avivilloz <avivilloz@gmail.com>
2024-07-08 18:42:19 +0300

    ur

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+terraform_env_names=("development" "production")
+
+if [ $# -ne 1 ]; then
+    echo "Provide terraform environment name as argument (options: ${terraform_env_names[@]})"
+    exit 1
+fi
+
+found="false"
+for option in "${terraform_env_names[@]}"; do
+  if [[ "$option" == "$1" ]]; then
+    found="true"
+    break
+  fi
+done
+
+if [[ $found == "false" ]]; then
+  echo "Error: '${1}' is not a valid option."
+  echo "Valid options are: ${terraform_env_names[@]}"
+  exit 1
+fi
+
 project_base_dir="/home/deck/git/cloud_resume"
 
-terraform_env="development"
+terraform_env=$1
 terraform_envs_dir="$project_base_dir/infra/terraform/environments"
 terraform_env_path="$terraform_envs_dir/$terraform_env"
 
@@ -19,7 +40,7 @@ terraform -chdir=$terraform_env_path apply -auto-approve -var="project_base_dir=
 ansible-playbook $ansible_tffiles_upload_path --extra-vars "project_base_dir=$project_base_dir"
 
 s3_bucket_name=$(terraform output -state=$terraform_env_path/terraform.tfstate -raw s3_bucket_name)
-api_stage_endpoint=$(terraform output -state=$terraform_env_path/terraform.tfstate -raw api_stage_endpoint)
+api_url=$(terraform output -state=$terraform_env_path/terraform.tfstate -raw api_url)
 cloudfront_distribution_id=$(terraform output -state=$terraform_env_path/terraform.tfstate -raw cloudfront_distribution_id)
 
-ansible-playbook $ansible_deploy_path --extra-vars "project_base_dir=$project_base_dir s3_bucket_name=$s3_bucket_name api_stage_endpoint=$api_stage_endpoint cloudfront_distribution_id=$cloudfront_distribution_id"
+ansible-playbook $ansible_deploy_path --extra-vars "project_base_dir=$project_base_dir s3_bucket_name=$s3_bucket_name api_url=$api_url cloudfront_distribution_id=$cloudfront_distribution_id"
