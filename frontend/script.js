b6cdabc30a9f6d3022cec4dd161ca5708e950c3e
avivilloz <avivilloz@gmail.com>
2024-07-10 11:09:15 +0300

    ur

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 async function setViewsCount() {
     const views = document.getElementById('views');
     try {
-        const getUrl = "__VIEWS_COUNT_API_ENDPOINT__";
+        const getUrl = "__API_URL__/views-count";
         const response = await fetch(getUrl);
         const data = await response.json();
         views.textContent = data.value.toString();
